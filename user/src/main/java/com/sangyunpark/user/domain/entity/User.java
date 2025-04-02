9c42aaaa03d3455c4fa927771b5929347f8441a8
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 07:36:43 +0900

    feat : User 엔티티에 password 필드 추가 (#1)

--- user/src/main/java/com/sangyunpark/user/domain/entity/User.java
+++ user/src/main/java/com/sangyunpark/user/domain/entity/User.java
@@ -5,12 +5,20 @@ import com.sangyunpark.user.domain.vo.RegisterType;
 import com.sangyunpark.user.domain.vo.UserStatus;
 import com.sangyunpark.user.domain.vo.UserType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User extends BaseEntity {
 
     @Id @GeneratedValue
@@ -22,6 +30,9 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
+    private String password;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
@@ -41,6 +52,13 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String phoneNumber;
 
+    @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserAddress> userAddress = new ArrayList<>();
+
+    public void addUserAddress(UserAddress address) {
+        this.userAddress.add(address);
+        address.setUser(this);
+    }
+
 }
