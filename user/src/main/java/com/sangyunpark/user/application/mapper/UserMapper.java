7802513f93aead1887263f9c1d6068e82ef8f888
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 07:38:02 +0900

    feat : UserSignupRequestDto → User 엔티티 변환 Mapper 구현 (#1)

---
+++ user/src/main/java/com/sangyunpark/user/application/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.sangyunpark.user.application.mapper;
+
+import com.sangyunpark.user.domain.dto.request.UserSignupRequestDto;
+import com.sangyunpark.user.domain.entity.User;
+import com.sangyunpark.user.domain.entity.UserAddress;
+import com.sangyunpark.user.domain.vo.UserStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserMapper {
+
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User toEntity(UserSignupRequestDto dto) {
+
+        UserAddress userAddress = UserAddressMapper.toEntity(dto.shippingInfo());
+
+        User user = User.builder()
+                .email(dto.email())
+                .username(dto.username())
+                .password(passwordEncoder.encode(dto.password()))
+                .registerType(dto.registerType())
+                .phoneNumber(dto.phoneNumber())
+                .userType(dto.userType())
+                .userStatus(UserStatus.ACTIVE)
+                .build();
+
+        user.addUserAddress(userAddress);
+
+        return user;
+    }
+}
