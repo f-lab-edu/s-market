d76d8993f9dc28414e0e0ca6dde9b52d898270cf
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 07:42:39 +0900

    feat : 회원가입 응답 메시지를 ResponseMessages enum으로 분리 (#1)

---
+++ user/src/main/java/com/sangyunpark/user/constant/ResponseMessages.java
@@ -0,0 +1,16 @@
+package com.sangyunpark.user.constant;
+
+public enum ResponseMessages {
+
+    SUCCESS_SIGNUP("회원가입이 완료되었습니다.");
+
+    private final String message;
+
+    ResponseMessages(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
