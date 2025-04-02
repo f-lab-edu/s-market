8529873245c88055ac9890dc9bb523b37f56cca8
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 07:42:51 +0900

    feat : UserController 구현 (#1)

--- user/src/main/java/com/sangyunpark/user/presentation/UserController.java
+++ user/src/main/java/com/sangyunpark/user/presentation/UserController.java
@@ -1,19 +1,28 @@
 package com.sangyunpark.user.presentation;
 
+import com.sangyunpark.user.application.UserService;
 import com.sangyunpark.user.domain.dto.request.UserSignupRequestDto;
 import com.sangyunpark.user.domain.dto.response.UserSignupResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.sangyunpark.user.constant.ResponseMessages.*;
+
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @PostMapping
-    public ResponseEntity<UserSignupResponseDto> signup(@RequestBody UserSignupRequestDto request) {
-        return ResponseEntity.ok(new UserSignupResponseDto());
+    public ResponseEntity<UserSignupResponseDto> signup(@Valid @RequestBody UserSignupRequestDto request) {
+        Long userId = userService.signup(request);
+        return ResponseEntity.ok(new UserSignupResponseDto(userId, SUCCESS_SIGNUP.message()));
     }
 }
