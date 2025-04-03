386c32930fd1f9d9783cc451cbb36c845967a679
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 11:02:09 +0900

    feat: 회원가입 시 중복 이메일 검사 로직 추가 (#1)

--- user/src/main/java/com/sangyunpark/user/application/UserService.java
+++ user/src/main/java/com/sangyunpark/user/application/UserService.java
@@ -3,22 +3,27 @@ package com.sangyunpark.user.application;
 import com.sangyunpark.user.application.mapper.UserMapper;
 import com.sangyunpark.user.domain.dto.request.UserSignupRequestDto;
 import com.sangyunpark.user.domain.entity.User;
+import com.sangyunpark.user.exception.UserDuplicateException;
 import com.sangyunpark.user.infrastructure.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.sangyunpark.user.constant.ExceptionMessages.*;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserMapper userMapper;
-    private final UserJpaRepository repository;
+    private final UserJpaRepository userJpaRepository;
 
     @Transactional
     public Long signup(UserSignupRequestDto userSignupRequestDto) {
         User user = userMapper.toEntity(userSignupRequestDto);
-        User savedUser = repository.save(user);
+        userJpaRepository.findUserByEmail(userSignupRequestDto.email())
+                .ifPresent(u -> { throw new UserDuplicateException(EXCEPTION_USER_DUPLICATE.message()); });
+        User savedUser = userJpaRepository.save(user);
 
         return savedUser.getId();
     }
