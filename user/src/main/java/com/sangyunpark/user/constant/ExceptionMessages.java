75337f9e138648a98c77150f1b470bb5a6ce7664
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 10:59:42 +0900

    feat: 중복 회원 예외 메시지 Enum에 추가 (#1)

---
+++ user/src/main/java/com/sangyunpark/user/constant/ExceptionMessages.java
@@ -0,0 +1,16 @@
+package com.sangyunpark.user.constant;
+
+public enum ExceptionMessages {
+
+    EXCEPTION_USER_DUPLICATE("이미 존재하는 회원입니다.");
+
+    private final String message;
+
+    ExceptionMessages(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
