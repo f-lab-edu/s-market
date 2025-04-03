c820fb00f3239264b94fed596d156cee65f928b1
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 10:13:47 +0900

    feat: 사용자 ID 및 이메일 기반 조회 서비스 로직 구현 (#3)

--- user/src/main/java/com/sangyunpark/user/application/UserService.java
+++ user/src/main/java/com/sangyunpark/user/application/UserService.java
@@ -1,4 +1,32 @@
 package com.sangyunpark.user.application;
 
+import com.sangyunpark.user.application.mapper.UserMapper;
+import com.sangyunpark.user.domain.dto.response.UserSelectResponseDto;
+import com.sangyunpark.user.domain.entity.User;
+import com.sangyunpark.user.exception.UserNotFoundException;
+import com.sangyunpark.user.infrastructure.repository.JpaUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.sangyunpark.user.constant.ExceptionMessages.*;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserMapper userMapper;
+    private final JpaUserRepository jpaUserRepository;
+
+    @Transactional(readOnly = true)
+    public UserSelectResponseDto findUserById(final Long userId) {
+        User user = jpaUserRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(EXCEPTION_NOT_FOUND_USER.message()));
+        return userMapper.toUserSelectResponseDto(user);
+    }
+
+    @Transactional(readOnly = true)
+    public UserSelectResponseDto findUserByEmail(final String email) {
+        User user = jpaUserRepository.findUserByEmail(email).orElseThrow(() -> new UserNotFoundException(EXCEPTION_NOT_FOUND_USER.message()));
+        return userMapper.toUserSelectResponseDto(user);
+    }
 }
