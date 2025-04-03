b3e67266b480337e6f127b1b91cfa1fe3c6dcb6f
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 11:01:49 +0900

    feat: UserJpaRepository에 이메일 기반 조회 메서드 추가 (#1)

--- user/src/main/java/com/sangyunpark/user/infrastructure/repository/UserJpaRepository.java
+++ user/src/main/java/com/sangyunpark/user/infrastructure/repository/UserJpaRepository.java
@@ -3,5 +3,9 @@ package com.sangyunpark.user.infrastructure.repository;
 import com.sangyunpark.user.domain.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
 }
