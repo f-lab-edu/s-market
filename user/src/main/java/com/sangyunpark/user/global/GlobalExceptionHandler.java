4186ae166ee8a4cdb9d615269545d6c79936afe6
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 10:10:30 +0900

    feat: GlobalExceptionHandler에서 UserNotFoundException 처리 로직 추가 (#3)

---
+++ user/src/main/java/com/sangyunpark/user/global/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.sangyunpark.user.global;
+
+import com.sangyunpark.user.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
