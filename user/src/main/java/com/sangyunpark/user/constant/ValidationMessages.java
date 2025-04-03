1eb0cd8271c11cba0b9c3bb24f32faec6346f7c2
sangyunpark99 <psangyun5108@gmail.com>
2025-04-03 11:47:28 +0900

    feat: 이메일 유효성 검사를 위한 메시지 상수 정의 (#3)

---
+++ user/src/main/java/com/sangyunpark/user/constant/ValidationMessages.java
@@ -0,0 +1,6 @@
+package com.sangyunpark.user.constant;
+
+public class ValidationMessages {
+    public static final String EMAIL_INVALID = "이메일 형식이 올바르지 않습니다.";
+    public static final String EMAIL_REQUIRED = "이메일은 필수로 입력해야 합니다.";
+}
