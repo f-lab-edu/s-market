ff8762a6f3a94fb6b72fb1cd577defdc29ddcde2
sangyunpark99 <psangyun5108@gmail.com>
2025-04-04 10:49:05 +0900

    refactor: 전역 예외 처리 방식 통합 및 BusinessException 기반 구조로 개선 (#1)

--- user/src/main/java/com/sangyunpark/user/global/GlobalExceptionHandler.java
+++ user/src/main/java/com/sangyunpark/user/global/GlobalExceptionHandler.java
@@ -1,16 +1,31 @@
 package com.sangyunpark.user.global;
 
-import com.sangyunpark.user.exception.UserDuplicateException;
-import org.springframework.http.HttpStatus;
+import com.sangyunpark.user.constant.code.ErrorCode;
+import com.sangyunpark.user.domain.dto.response.ErrorResponse;
+import com.sangyunpark.user.exception.BusinessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserDuplicateException.class)
-    public ResponseEntity<String> handleUserDuplicateException(UserDuplicateException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorCode errorCode  = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(errorCode.getStatus()).body(new ErrorResponse(errorCode.getCode()));
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        return ResponseEntity.status(errorCode.getStatus()).body(new ErrorResponse(errorCode.getCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
+        return ResponseEntity.status(errorCode.getStatus()).body(new ErrorResponse(errorCode.getCode()));
     }
 }
