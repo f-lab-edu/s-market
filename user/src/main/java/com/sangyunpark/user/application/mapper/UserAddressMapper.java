119f3432512905367195361ba76ffc33cbc4a5f3
sangyunpark99 <psangyun5108@gmail.com>
2025-04-04 10:50:56 +0900

    refactor: UserAddressMapper 유틸성 클래스로 변경 및 @Component 제거, 파라미터 변수에 final 키워드 추가 (#1)

--- user/src/main/java/com/sangyunpark/user/application/mapper/UserAddressMapper.java
+++ user/src/main/java/com/sangyunpark/user/application/mapper/UserAddressMapper.java
@@ -2,12 +2,10 @@ package com.sangyunpark.user.application.mapper;
 
 import com.sangyunpark.user.domain.dto.request.UserAddressRequestDto;
 import com.sangyunpark.user.domain.entity.UserAddress;
-import org.springframework.stereotype.Component;
 
-@Component
 public class UserAddressMapper {
 
-    public static UserAddress toEntity(UserAddressRequestDto dto) {
+    public static UserAddress toEntity(final UserAddressRequestDto dto) {
         return UserAddress.builder()
                 .address(dto.address())
                 .defaultAddress(dto.defaultAddress())
