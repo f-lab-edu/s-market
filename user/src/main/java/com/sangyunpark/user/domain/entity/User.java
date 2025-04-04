114339ec3a00816a6785a2e1e370ed4df86cf230
sangyunpark99 <psangyun5108@gmail.com>
2025-04-04 10:49:28 +0900

    refactor: BaseEntity 삭제 후, 필드에 createdAt, updatedAt 변수 추가 (#1)

--- user/src/main/java/com/sangyunpark/user/domain/entity/User.java
+++ user/src/main/java/com/sangyunpark/user/domain/entity/User.java
@@ -1,6 +1,5 @@
 package com.sangyunpark.user.domain.entity;
 
-import com.sangyunpark.user.common.BaseEntity;
 import com.sangyunpark.user.constant.enums.RegisterType;
 import com.sangyunpark.user.constant.enums.UserStatus;
 import com.sangyunpark.user.constant.enums.UserType;
@@ -9,17 +8,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User extends BaseEntity {
+public class User {
 
     @Id @GeneratedValue
     @Column(name = "user_id")
@@ -56,9 +60,16 @@ public class User extends BaseEntity {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserAddress> userAddress = new ArrayList<>();
 
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
     public void addUserAddress(UserAddress address) {
         this.userAddress.add(address);
         address.setUser(this);
     }
-
 }
