ff79ca6ed1e0245c7361b3a1b94d6b71cbe7e275
sangyunpark99 <psangyun5108@gmail.com>
2025-04-04 10:55:05 +0900

    refactor: 회원가입 로직 함수형 스타일로 리팩터링 및 예외 처리 일관화 (#1)

--- user/src/main/java/com/sangyunpark/user/application/UserService.java
+++ user/src/main/java/com/sangyunpark/user/application/UserService.java
@@ -1,30 +1,32 @@
 package com.sangyunpark.user.application;
 
-import com.sangyunpark.user.application.mapper.UserMapper;
+import com.sangyunpark.user.constant.code.ErrorCode;
 import com.sangyunpark.user.domain.dto.request.UserSignupRequestDto;
 import com.sangyunpark.user.domain.entity.User;
-import com.sangyunpark.user.exception.UserDuplicateException;
+import com.sangyunpark.user.exception.BusinessException;
 import com.sangyunpark.user.infrastructure.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.sangyunpark.user.constant.message.ExceptionMessages.*;
+import static com.sangyunpark.user.application.mapper.UserMapper.toEntity;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserMapper userMapper;
     private final UserJpaRepository userJpaRepository;
 
     @Transactional
-    public Long signup(UserSignupRequestDto userSignupRequestDto) {
-        User user = userMapper.toEntity(userSignupRequestDto);
-        userJpaRepository.findUserByEmail(userSignupRequestDto.email())
-                .ifPresent(u -> { throw new UserDuplicateException(EXCEPTION_USER_DUPLICATE.message()); });
-        User savedUser = userJpaRepository.save(user);
+    public Long signup(final UserSignupRequestDto userSignupRequestDto) {
 
-        return savedUser.getId();
+        return (Long) userJpaRepository.findUserByEmail(userSignupRequestDto.email())
+                .map(user -> {
+                    throw new BusinessException(ErrorCode.USER_DUPLICATE);
+                })
+                .orElseGet(() -> {
+                    User savedUser = userJpaRepository.save(toEntity(userSignupRequestDto));
+                    return savedUser.getId();
+                });
     }
 }
