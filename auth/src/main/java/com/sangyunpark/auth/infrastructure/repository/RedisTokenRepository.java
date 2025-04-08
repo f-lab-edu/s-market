2fd95409969cae7f86efe0522fa65b88377a183b
sangyunpark99 <psangyun5108@gmail.com>
2025-04-08 10:33:20 +0900

    feat: Redis 기반 리프레시 토큰 저장소 구현 (#5)

---
+++ auth/src/main/java/com/sangyunpark/auth/infrastructure/repository/RedisTokenRepository.java
@@ -0,0 +1,36 @@
+package com.sangyunpark.auth.infrastructure.repository;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@Repository
+public class RedisTokenRepository {
+
+    private final StringRedisTemplate redisTemplate;
+    private final long refreshTokenExpireTime;
+
+    public RedisTokenRepository(final StringRedisTemplate redisTemplate, @Value("${jwt.refreshToken-expiration}") final Long expireTime) {
+        this.redisTemplate = redisTemplate;
+        this.refreshTokenExpireTime = expireTime;
+    }
+
+    public void save(final String email, final String refreshToken) {
+        redisTemplate.opsForValue().set(email, refreshToken, refreshTokenExpireTime, TimeUnit.MILLISECONDS);
+    }
+
+    public Optional<String> findByEmail(final String email) {
+        return Optional.ofNullable(redisTemplate.opsForValue().get(email));
+    }
+
+    public void delete(final String email) {
+        redisTemplate.delete(email);
+    }
+
+    public boolean exists(final String email) {
+        return redisTemplate.hasKey(email);
+    }
+}
