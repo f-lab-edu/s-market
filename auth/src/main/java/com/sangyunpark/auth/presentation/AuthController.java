9c63d8bc91ca579ff9d4ef795277f7d64cfdde01
sangyunpark99 <psangyun5108@gmail.com>
2025-04-08 14:58:55 +0900

    feat: 토큰 재발급 API 구현 (#5)

--- auth/src/main/java/com/sangyunpark/auth/presentation/AuthController.java
+++ auth/src/main/java/com/sangyunpark/auth/presentation/AuthController.java
@@ -1,14 +1,13 @@
 package com.sangyunpark.auth.presentation;
 
 import com.sangyunpark.auth.application.AuthService;
+import com.sangyunpark.auth.jwt.UserPrincipal;
 import com.sangyunpark.auth.presentation.dto.request.LoginRequestDto;
-import com.sangyunpark.auth.presentation.dto.response.LoginResponseDto;
+import com.sangyunpark.auth.presentation.dto.response.TokenResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -18,7 +17,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public LoginResponseDto login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
+    public TokenResponseDto login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
         return authService.login(loginRequestDto);
+    }
+
+    @PostMapping("/reissue")
+    public TokenResponseDto reissue(@RequestHeader("X-Refresh-Token") final String refreshToken, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return authService.reissue(refreshToken, userPrincipal);
     }
 }
