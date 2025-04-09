9cedb99bf5458f8652bdd16d8e833b1504154dd5
sangyunpark99 <psangyun5108@gmail.com>
2025-04-09 11:08:58 +0900

    test: 로그아웃 테스트 코드 추가 (#5)

--- auth/src/test/java/com/sangyunpark/auth/infrastructure/repository/RedisTokenRepositoryTest.java
+++ auth/src/test/java/com/sangyunpark/auth/infrastructure/repository/RedisTokenRepositoryTest.java
@@ -16,6 +16,8 @@ class RedisTokenRepositoryTest {
 
     private static final String EMAIL = "test@example.com";
     private static final String REFRESH_TOKEN = "refreshToken123";
+    private static final String TEST_ACCESS_TOKEN = "test-access-token";
+    private static final long REMAINING_TIME = 500L;
 
     @Autowired
     private RedisTokenRepository redisTokenRepository;
@@ -37,6 +39,44 @@ class RedisTokenRepositoryTest {
         redisTokenRepository.delete(EMAIL);
 
         assertThat(redisTokenRepository.exists(EMAIL)).isFalse();
+    }
+
+    @Test
+    @DisplayName("로그아웃시 토큰이 Redis에 저장된다.")
+    void 로그아웃시_토큰이_레디스에_저장() {
+        // given, when
+        redisTokenRepository.saveLogOutToken(TEST_ACCESS_TOKEN, REMAINING_TIME);
+
+        // then
+        assertThat(redisTokenRepository.exists(TEST_ACCESS_TOKEN)).isTrue();
+    }
+
+    @Test
+    @DisplayName("로그아웃 토큰이 Redis에 존재하지 않으면 false를 반환한다.")
+    void 로그아웃시_토큰이_레디스에_저장된_경우_true_반환() {
+        // given
+        redisTokenRepository.saveLogOutToken(TEST_ACCESS_TOKEN, REMAINING_TIME);
+
+        // when
+        boolean result = redisTokenRepository.isLogOutToken(TEST_ACCESS_TOKEN);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("로그아웃시 블랙리스트에 등록된 토큰이 만료된 경우 false를 반환한다.")
+    void 로그아웃시_블랙리스트에_등록된_토큰이_만료된_경우_false_반환() throws InterruptedException {
+        //given
+        redisTokenRepository.saveLogOutToken(TEST_ACCESS_TOKEN, 1L);
+
+        Thread.sleep(3L);
+
+        // when
+        boolean result = redisTokenRepository.isLogOutToken(TEST_ACCESS_TOKEN);
+
+        // then
+        assertThat(result).isFalse();
     }
 
     @AfterEach
