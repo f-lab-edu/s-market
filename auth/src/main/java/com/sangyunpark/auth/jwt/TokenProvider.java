917f537d90651ae6dcbe5659ad9d7a98bf898bb4
sangyunpark99 <psangyun5108@gmail.com>
2025-04-14 08:52:01 +0900

    feat: 예외 발생 시 에러 메시지 확인 로깅 추가 (#5)

--- auth/src/main/java/com/sangyunpark/auth/jwt/TokenProvider.java
+++ auth/src/main/java/com/sangyunpark/auth/jwt/TokenProvider.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class TokenProvider {
 
@@ -60,6 +62,7 @@ public class TokenProvider {
                     .parseClaimsJws(token);
 
         }catch (final JwtException | IllegalArgumentException e) {
+            log.error(e.getMessage());
             return false;
         }
 
