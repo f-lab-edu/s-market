76c17f5c41cffb8d9da67796a4a8ecbc06bc6129
sangyunpark99 <psangyun5108@gmail.com>
2025-04-14 15:22:13 +0900

    feat: 인증 필터 리펙토링 및 관리자 인가, 로그아웃 처리 로직 추가 (#7)

---
+++ gateway/src/main/java/org/sangyunpark/gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,72 @@
+package org.sangyunpark.gateway.filter;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.sangyunpark.gateway.application.AuthenticationService;
+import org.sangyunpark.gateway.constant.code.ErrorCode;
+import org.sangyunpark.gateway.constant.code.UserType;
+import org.sangyunpark.gateway.jwt.TokenProvider;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import static org.sangyunpark.gateway.filter.matcher.UrlMatcher.isAdminUrl;
+import static org.sangyunpark.gateway.filter.matcher.UrlMatcher.isLogOutUrl;
+import static org.sangyunpark.gateway.filter.matcher.WhitelistMatcher.isWhitelisted;
+import static org.sangyunpark.gateway.filter.utils.HttpResponseUtils.unauthorized;
+import static org.sangyunpark.gateway.filter.utils.RequestMutationUtils.mutateRequestWithClaims;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter implements GlobalFilter {
+
+    private static final String USER_TYPE = "userType";
+    private final AuthenticationService authenticationService;
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public Mono<Void> filter(final ServerWebExchange exchange, final GatewayFilterChain chain) {
+        final ServerHttpRequest request = exchange.getRequest();
+
+        if (isWhitelisted(request)) {
+            return chain.filter(exchange);
+        }
+
+        final String token = tokenProvider.resolveToken(request);
+        if (token == null) {
+            return unauthorized(exchange, ErrorCode.INVALID_TOKEN);
+        }
+
+        if(isLogOutUrl(request)) {
+           return handleLogoutRequest(token, chain, exchange);
+        }
+
+        return handleAuthorizedRequest(token, chain, exchange);
+    }
+
+    private Mono<Void> handleLogoutRequest(final String token, final GatewayFilterChain chain, final ServerWebExchange exchange) {
+        return authenticationService.extractClaims(token)
+                .flatMap(claims -> chain.filter(exchange))
+                .onErrorResume(e -> unauthorized(exchange, ErrorCode.INVALID_TOKEN));
+    }
+
+    private Mono<Void> handleAuthorizedRequest(final String token, final GatewayFilterChain chain, final ServerWebExchange exchange) {
+        return authenticationService.extractClaims(token)
+                .flatMap(claims -> authorizeAndMutateRequest(exchange, chain, claims))
+                .onErrorResume(e -> unauthorized(exchange, ErrorCode.INVALID_TOKEN));
+    }
+
+    private Mono<Void> authorizeAndMutateRequest(final ServerWebExchange exchange, final GatewayFilterChain chain, final Claims claims) {
+        final ServerHttpRequest request = exchange.getRequest();
+        final String role = claims.get(USER_TYPE, String.class);
+
+        if (isAdminUrl(request) && !UserType.ADMIN.name().equals(role)) {
+            return unauthorized(exchange,ErrorCode.INVALID_TOKEN);
+        }
+
+        return chain.filter(mutateRequestWithClaims(exchange, claims));
+    }
+}
