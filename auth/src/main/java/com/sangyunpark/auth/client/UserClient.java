3b137c77100ba69a975a9899ac65476645130d8f
sangyunpark99 <psangyun5108@gmail.com>
2025-04-14 09:13:51 +0900

    feat: userClient 실패시 실행될 Fallback메서드 추가 (#5)

--- auth/src/main/java/com/sangyunpark/auth/client/UserClient.java
+++ auth/src/main/java/com/sangyunpark/auth/client/UserClient.java
@@ -1,15 +1,15 @@
 package com.sangyunpark.auth.client;
 
 import com.sangyunpark.auth.client.dto.response.FeignUserResponseDto;
+import com.sangyunpark.auth.client.fallback.UserClientFallbackFactory;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.constraints.Email;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", url = "${service.user.url}")
+@FeignClient(name = "user-service", url = "${service.user.url}",fallbackFactory = UserClientFallbackFactory.class)
 public interface UserClient {
-
     @GetMapping("/api/v1/users")
     @CircuitBreaker(name = "userService")
     FeignUserResponseDto findUserByEmail(@RequestParam("email") @Email String email);
