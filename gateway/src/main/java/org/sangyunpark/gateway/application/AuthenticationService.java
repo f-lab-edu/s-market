7f84e7ea9003ed00266c0c780ab198042722990d
sangyunpark99 <psangyun5108@gmail.com>
2025-04-15 13:33:13 +0900

    refactor: 인증 서비스 캐싱 및 블랙리스트 로직 수정 (#7)

--- gateway/src/main/java/org/sangyunpark/gateway/application/AuthenticationService.java
+++ gateway/src/main/java/org/sangyunpark/gateway/application/AuthenticationService.java
@@ -1,26 +1,81 @@
 package org.sangyunpark.gateway.application;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
-import org.sangyunpark.gateway.infrastructure.redis.TokenBlackListRepository;
+import org.sangyunpark.gateway.filter.dto.CachedUser;
+import org.sangyunpark.gateway.infrastructure.redis.RedisTokenRepository;
 import org.sangyunpark.gateway.jwt.TokenProvider;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final TokenProvider tokenProvider;
-    private final TokenBlackListRepository tokenBlackListRepository;
+    private static final String AUTH_TOKEN_KEY = "auth_token:";
+    private static final String USER_TYPE = "userType";
+    private static final String USER_STATUS = "userStatus";
 
-    public Mono<Claims> extractClaims(final String token) {
-        return tokenBlackListRepository.isBlackList(token)
-                .flatMap(isBlackList -> {
-                    if(isBlackList) {
-                       return  Mono.error(new RuntimeException());
+    private final TokenProvider tokenProvider;
+    private final RedisTokenRepository redisTokenRepository;
+    private final ObjectMapper objectMapper;
+
+    public Mono<Void> logout(final String token) {
+        return checkBlackList(token, redisTokenRepository.removeAuthToken(token).then());
+    }
+
+    public Mono<CachedUser> getAuthenticatedUser(final String token) {
+        return checkBlackList(token, Mono.defer(() -> getCachedUser(token).switchIfEmpty(parseAndCacheUser(token))));
+    }
+
+    private <T> Mono<T> checkBlackList(String token, Mono<T> nextAction) {
+        return redisTokenRepository.isBlackList(token)
+                .flatMap(isBlacklisted -> {
+                    if (isBlacklisted) {
+                        return Mono.error(new RuntimeException());
                     }
-                    return Mono.fromCallable(() -> tokenProvider.parseClaims(token));
+                    return nextAction;
                 });
+    }
+
+    public Mono<CachedUser> getCachedUser(final String token) {
+        return redisTokenRepository.getCachedUser(token)
+                .flatMap(json -> {
+                    try {
+                        CachedUser cachedUser = objectMapper.readValue(json, CachedUser.class);
+                        return Mono.just(cachedUser);
+                    }catch (Exception e) {
+                        return Mono.error(new RuntimeException());
+                    }
+                });
+    }
+
+    public Mono<CachedUser> parseAndCacheUser(final String token) {
+        return  parseClaims(token)
+                .flatMap(cachedUser -> cacheUser(token, cachedUser).thenReturn(cachedUser));
+    }
+
+    public Mono<CachedUser> parseClaims(final String token) {
+        return Mono.fromCallable(() -> {
+            Claims claims = tokenProvider.parseClaims(token);
+            return new CachedUser(
+                    claims.getSubject(),
+                    claims.get(USER_TYPE, String.class),
+                    claims.get(USER_STATUS, String.class)
+            );
+        });
+    }
+
+    public Mono<Boolean> cacheUser(final String token, final CachedUser cachedUser) {
+        try {
+            final String key = AUTH_TOKEN_KEY + token;
+            final String value = objectMapper.writeValueAsString(cachedUser);
+            return redisTokenRepository.saveAuthToken(key, value, Duration.ofMinutes(10));
+        }catch (Exception e) {
+            return Mono.error(new RuntimeException());
+        }
     }
 }
