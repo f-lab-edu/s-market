faf70705ad2a85fb909b29e29f50e80bd9961ce1
sangyunpark99 <psangyun5108@gmail.com>
2025-04-15 13:38:32 +0900

    refactor: CachedUser로 헤더에 첨부할 값 가져오기 (#7)

--- gateway/src/main/java/org/sangyunpark/gateway/filter/utils/RequestMutationUtils.java
+++ gateway/src/main/java/org/sangyunpark/gateway/filter/utils/RequestMutationUtils.java
@@ -1,6 +1,6 @@
 package org.sangyunpark.gateway.filter.utils;
 
-import io.jsonwebtoken.Claims;
+import org.sangyunpark.gateway.filter.dto.CachedUser;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 
@@ -9,14 +9,12 @@ public class RequestMutationUtils {
     public static final String X_USER_EMAIL = "X-User-Email";
     public static final String X_USER_TYPE = "X-User-Type";
     public static final String X_USER_STATUS = "X-User-Status";
-    private static final String USER_TYPE = "userType";
-    private static final String USER_STATUS = "userStatus";
 
-    public static ServerWebExchange mutateRequestWithClaims(ServerWebExchange exchange, Claims claims) {
+    public static ServerWebExchange mutateRequestWithClaims(final ServerWebExchange exchange, final CachedUser user) {
         final ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
-                .header(X_USER_EMAIL, claims.getSubject())
-                .header(X_USER_TYPE, claims.get(USER_TYPE, String.class))
-                .header(X_USER_STATUS, claims.get(USER_STATUS, String.class))
+                .header(X_USER_EMAIL, user.email())
+                .header(X_USER_TYPE, user.userType())
+                .header(X_USER_STATUS, user.userStatus())
                 .build();
 
         final ServerWebExchange mutatedExchange = exchange.mutate()
