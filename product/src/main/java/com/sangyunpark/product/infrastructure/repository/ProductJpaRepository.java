5be06e44851507cd6f0011cc9a463a5d043783fc
sangyunpark99 <psangyun5108@gmail.com>
2025-04-17 14:55:07 +0900

    refactor: ProductJpaRepository 불필요한 메서드 제거 (#8)

--- product/src/main/java/com/sangyunpark/product/infrastructure/repository/ProductJpaRepository.java
+++ product/src/main/java/com/sangyunpark/product/infrastructure/repository/ProductJpaRepository.java
@@ -1,16 +1,8 @@
 package com.sangyunpark.product.infrastructure.repository;
 
 import com.sangyunpark.product.domain.entity.Product;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p WHERE p.createdAt < :cursor AND p.startAt <= :now AND p.endAt >: now ORDER BY p.createdAt DESC")
-    List<Product> findAllByCursor(@Param("cursor") LocalDateTime cursor, @Param("now") LocalDateTime now, Pageable pageable);
 }
