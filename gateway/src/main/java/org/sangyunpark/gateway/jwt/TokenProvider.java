f33f8aec3577d8075feacf22b60e0e3fc29725fb
sangyunpark99 <psangyun5108@gmail.com>
2025-04-19 23:12:52 +0900

    feat: null 반환 → 빈 문자열 반환 (#7)

--- gateway/src/main/java/org/sangyunpark/gateway/jwt/TokenProvider.java
+++ gateway/src/main/java/org/sangyunpark/gateway/jwt/TokenProvider.java
@@ -3,6 +3,7 @@ package org.sangyunpark.gateway.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -13,7 +14,7 @@ import javax.crypto.SecretKey;
 @Component
 public class TokenProvider {
 
-    private static final String BEARER_PREFIX = "Bearer";
+    private final String BEARER_PREFIX = "Bearer";
 
     private final SecretKey secretKey;
 
@@ -23,9 +24,11 @@ public class TokenProvider {
 
     public String resolveToken(final ServerHttpRequest request) {
         final String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
-            return null;
+
+        if(!StringUtils.startsWith(authHeader, BEARER_PREFIX)) {
+            return "";
         }
+
         return authHeader.substring(BEARER_PREFIX.length()).trim();
     }
 
