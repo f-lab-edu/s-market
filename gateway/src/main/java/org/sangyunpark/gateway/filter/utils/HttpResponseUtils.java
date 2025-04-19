a1740e636bf04bc3798422137c412f203ea6f82d
sangyunpark99 <psangyun5108@gmail.com>
2025-04-19 22:38:32 +0900

    refactor: 메서드 static 제거 (#7)

--- gateway/src/main/java/org/sangyunpark/gateway/filter/utils/HttpResponseUtils.java
+++ gateway/src/main/java/org/sangyunpark/gateway/filter/utils/HttpResponseUtils.java
@@ -4,15 +4,17 @@ import org.sangyunpark.gateway.constant.code.ErrorCode;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class HttpResponseUtils {
 
-    private static final String JSON_CONTENT_TYPE = "application/json";
-    private static final String ERROR_RESPONSE_FORMAT = "{\"code\":\"%s\"}";
+    private final String JSON_CONTENT_TYPE = "application/json";
+    private final String ERROR_RESPONSE_FORMAT = "{\"code\":\"%s\"}";
 
-    public static Mono<Void> unauthorized(final ServerWebExchange exchange, final ErrorCode errorCode) {
+    public Mono<Void> unauthorized(final ServerWebExchange exchange, final ErrorCode errorCode) {
         final ServerHttpResponse response = exchange.getResponse();
         setJsonHeaders(response, errorCode);
         final byte[] bytes = createErrorBody(errorCode).getBytes();
@@ -20,12 +22,12 @@ public class HttpResponseUtils {
         return response.writeWith(Mono.just(buffer));
     }
 
-    public static void setJsonHeaders(ServerHttpResponse response, ErrorCode errorCode) {
+    public void setJsonHeaders(ServerHttpResponse response, ErrorCode errorCode) {
         response.setStatusCode(errorCode.getStatus());
         response.getHeaders().add(HttpHeaders.CONTENT_TYPE, JSON_CONTENT_TYPE);
     }
 
-    private static String createErrorBody(ErrorCode errorCode) {
+    private String createErrorBody(ErrorCode errorCode) {
         return ERROR_RESPONSE_FORMAT.formatted(errorCode.getCode());
     }
 }
