8905464a17432e8acf83f486ea586bdaf5b4b176
sangyunpark99 <psangyun5108@gmail.com>
2025-04-19 23:11:58 +0900

    refactor: static 키워드 제거 (#7)

--- gateway/src/main/java/org/sangyunpark/gateway/filter/utils/RequestMutationUtils.java
+++ gateway/src/main/java/org/sangyunpark/gateway/filter/utils/RequestMutationUtils.java
@@ -2,15 +2,17 @@ package org.sangyunpark.gateway.filter.utils;
 
 import org.sangyunpark.gateway.filter.dto.CachedUser;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
+@Component
 public class RequestMutationUtils {
 
-    public static final String X_USER_EMAIL = "X-User-Email";
-    public static final String X_USER_TYPE = "X-User-Type";
-    public static final String X_USER_STATUS = "X-User-Status";
+    public final String X_USER_EMAIL = "X-User-Email";
+    public final String X_USER_TYPE = "X-User-Type";
+    public final String X_USER_STATUS = "X-User-Status";
 
-    public static ServerWebExchange mutateRequestWithClaims(final ServerWebExchange exchange, final CachedUser user) {
+    public ServerWebExchange mutateRequestWithClaims(final ServerWebExchange exchange, final CachedUser user) {
         final ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
                 .header(X_USER_EMAIL, user.email())
                 .header(X_USER_TYPE, user.userType())
