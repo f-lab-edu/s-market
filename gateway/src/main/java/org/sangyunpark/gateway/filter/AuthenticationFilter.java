8a7c7e7eb44846f0a834c4526589f1d7eaa1e297
sangyunpark99 <psangyun5108@gmail.com>
2025-04-19 22:36:02 +0900

    refactor: static 제거 (#7)

--- gateway/src/main/java/org/sangyunpark/gateway/filter/AuthenticationFilter.java
+++ gateway/src/main/java/org/sangyunpark/gateway/filter/AuthenticationFilter.java
@@ -6,6 +6,10 @@ import org.sangyunpark.gateway.application.AuthenticationService;
 import org.sangyunpark.gateway.constant.code.ErrorCode;
 import org.sangyunpark.gateway.constant.code.UserType;
 import org.sangyunpark.gateway.filter.dto.CachedUser;
+import org.sangyunpark.gateway.filter.matcher.UrlMatcher;
+import org.sangyunpark.gateway.filter.matcher.WhitelistMatcher;
+import org.sangyunpark.gateway.filter.utils.HttpResponseUtils;
+import org.sangyunpark.gateway.filter.utils.RequestMutationUtils;
 import org.sangyunpark.gateway.jwt.TokenProvider;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -14,12 +18,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import static org.sangyunpark.gateway.filter.matcher.UrlMatcher.isAdminUrl;
-import static org.sangyunpark.gateway.filter.matcher.UrlMatcher.isLogOutUrl;
-import static org.sangyunpark.gateway.filter.matcher.WhitelistMatcher.isWhitelisted;
-import static org.sangyunpark.gateway.filter.utils.HttpResponseUtils.unauthorized;
-import static org.sangyunpark.gateway.filter.utils.RequestMutationUtils.mutateRequestWithClaims;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -27,22 +25,26 @@ public class AuthenticationFilter implements GlobalFilter {
 
     private final AuthenticationService authenticationService;
     private final TokenProvider tokenProvider;
+    private final WhitelistMatcher whitelistMatcher;
+    private final HttpResponseUtils httpResponseUtils;
+    private final UrlMatcher urlMatcher;
+    private final RequestMutationUtils requestMutationUtils;
 
     @Override
     public Mono<Void> filter(final ServerWebExchange exchange, final GatewayFilterChain chain) {
         final ServerHttpRequest request = exchange.getRequest();
 
-        if (isWhitelisted(request)) {
+        if (whitelistMatcher.isWhitelisted(request)) {
             return chain.filter(exchange);
         }
 
         final String token = tokenProvider.resolveToken(request);
-        if (token == null) {
-            return unauthorized(exchange, ErrorCode.INVALID_TOKEN);
+        if (token.isBlank()) {
+            return httpResponseUtils.unauthorized(exchange, ErrorCode.INVALID_TOKEN);
         }
 
-        if(isLogOutUrl(request)) {
-           return handleLogoutRequest(token, chain, exchange);
+        if (urlMatcher.isLogOutUrl(request)) {
+            return handleLogoutRequest(token, chain, exchange);
         }
 
         return handleAuthorizedRequest(token, chain, exchange);
@@ -50,24 +52,24 @@ public class AuthenticationFilter implements GlobalFilter {
 
     private Mono<Void> handleLogoutRequest(final String token, final GatewayFilterChain chain, final ServerWebExchange exchange) {
         return authenticationService.logout(token)
-                .then(Mono.defer(() -> chain.filter(exchange)))
-                .onErrorResume(e -> unauthorized(exchange, ErrorCode.INVALID_TOKEN));
+                .then(chain.filter(exchange))
+                .onErrorResume(e -> httpResponseUtils.unauthorized(exchange, ErrorCode.INVALID_TOKEN));
     }
 
     private Mono<Void> handleAuthorizedRequest(final String token, final GatewayFilterChain chain, final ServerWebExchange exchange) {
         return authenticationService.getAuthenticatedUser(token)
                 .flatMap(cachedUser -> authorizeAndMutateRequest(exchange, chain, cachedUser))
-                .onErrorResume(e -> unauthorized(exchange, ErrorCode.INVALID_TOKEN));
+                .onErrorResume(e -> httpResponseUtils.unauthorized(exchange, ErrorCode.INVALID_TOKEN));
     }
 
     private Mono<Void> authorizeAndMutateRequest(final ServerWebExchange exchange, final GatewayFilterChain chain, final CachedUser user) {
         final ServerHttpRequest request = exchange.getRequest();
         final String role = user.userType();
 
-        if (isAdminUrl(request) && !UserType.ADMIN.name().equals(role)) {
-            return unauthorized(exchange,ErrorCode.INVALID_TOKEN);
+        if (urlMatcher.isAdminUrl(request) && !UserType.ADMIN.name().equals(role)) {
+            return httpResponseUtils.unauthorized(exchange, ErrorCode.INVALID_TOKEN);
         }
 
-        return chain.filter(mutateRequestWithClaims(exchange, user));
+        return chain.filter(requestMutationUtils.mutateRequestWithClaims(exchange, user));
     }
 }
