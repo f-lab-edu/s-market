f9184dc329af68b52ceb7b9980303e7f2a07ce88
sangyunpark99 <psangyun5108@gmail.com>
2025-04-23 23:08:12 +0900

    feat: 재고 감소 이벤트 consumer 구현 (#8)

---
+++ product/src/main/java/com/sangyunpark/product/infrastructure/kafka/StockEventConsumer.java
@@ -0,0 +1,44 @@
+package com.sangyunpark.product.infrastructure.kafka;
+
+import com.sangyunpark.product.application.event.StockDeductedEvent;
+import com.sangyunpark.product.constant.ErrorCode;
+import com.sangyunpark.product.exception.BusinessException;
+import com.sangyunpark.product.infrastructure.redis.OrderDuplicationRepository;
+import com.sangyunpark.product.infrastructure.redis.StockRedisRepository;
+import com.sangyunpark.product.infrastructure.repository.StockJpaRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StockEventConsumer {
+
+    private final String TOPIC = "stock.deducted";
+    private final String GROUP_ID = "product-service";
+
+    private final StockJpaRepository stockJpaRepository;
+    private final OrderDuplicationRepository orderDuplicationRepository;
+    private final StockRedisRepository stockRedisRepository;
+
+    @Transactional
+    @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
+    public void consumeStockDeductedEvent(final StockDeductedEvent event) {
+
+        if(orderDuplicationRepository.isAlreadyProcessed(event.orderId())) {
+            log.info("이미 처리된 주문 orderId: {}", event.orderId());
+            stockRedisRepository.increase(event.productId(), event.quantity());
+            return;
+        }
+
+        int updatedRows = stockJpaRepository.decreaseStock(event.productId(), event.quantity());
+        if(updatedRows == 0) {
+            throw new BusinessException(ErrorCode.STOCK_NOT_ENOUGH);
+        }
+
+        orderDuplicationRepository.saveProcessed(event.orderId());
+    }
+}
