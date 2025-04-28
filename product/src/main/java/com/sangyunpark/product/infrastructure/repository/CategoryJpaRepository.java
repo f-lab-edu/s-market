5bd464a5aa21d8daf7a88d778e333e4cc1010057
sangyunpark99 <psangyun5108@gmail.com>
2025-04-28 14:56:03 +0900

    feat: N+1 문제 방지를 위한 fetch Join 쿼리 생성 (#8)

--- product/src/main/java/com/sangyunpark/product/infrastructure/repository/CategoryJpaRepository.java
+++ product/src/main/java/com/sangyunpark/product/infrastructure/repository/CategoryJpaRepository.java
@@ -2,6 +2,13 @@ package com.sangyunpark.product.infrastructure.repository;
 
 import com.sangyunpark.product.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CategoryJpaRepository extends JpaRepository<Category, Long> {
+
+    @Query("select c from Category c left join fetch c.children where c.id = :id")
+    Optional<Category> findWithChildrenById(@Param("id") Long id);
 }
