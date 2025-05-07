80ae2f4c74b7cceaa72ebcb4e1e2fe4882348149
sangyunpark99 <psangyun5108@gmail.com>
2025-05-07 09:33:45 +0900

    feat: 재고 증가/감소 Kafka 이벤트 발행 기능 구현 (#13)

--- product/src/main/java/com/sangyunpark/product/infrastructure/kafka/StockEventProducer.java
+++ product/src/main/java/com/sangyunpark/product/infrastructure/kafka/StockEventProducer.java
@@ -1,6 +1,7 @@
 package com.sangyunpark.product.infrastructure.kafka;
 
-import com.sangyunpark.product.application.event.StockDeductedEvent;
+import com.sangyunpark.product.infrastructure.kafka.event.StockDeductedEvent;
+import com.sangyunpark.product.infrastructure.kafka.event.StockIncreasedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -13,12 +14,23 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class StockEventProducer {
 
-    private final String TOPIC = "stock.deducted";
-    private final KafkaTemplate<String, StockDeductedEvent> kafkaTemplate;
+    private final String INCR_TOPIC = "stock.increased";
+    private final String DECR_TOPIC = "stock.deducted";
+    private final KafkaTemplate<String, StockDeductedEvent> kafkaDecrTemplate;
+    private final KafkaTemplate<String, StockIncreasedEvent> kafkaIncrTemplate;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void sendStockDeductedEvent(final StockDeductedEvent event) {
-        kafkaTemplate.send(TOPIC, String.valueOf(event.productId()), event)
+        kafkaDecrTemplate.send(DECR_TOPIC, String.valueOf(event.productId()), event)
+                .exceptionally(ex -> {
+                    log.error("Kafka DB 재고 동기화 메시지 발행 실패");
+                    return null;
+                });
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void sendStockIncreasedEvent(final StockIncreasedEvent event) {
+        kafkaIncrTemplate.send(INCR_TOPIC, String.valueOf(event.productId()), event)
                 .exceptionally(ex -> {
                     log.error("Kafka DB 재고 동기화 메시지 발행 실패");
                     return null;
