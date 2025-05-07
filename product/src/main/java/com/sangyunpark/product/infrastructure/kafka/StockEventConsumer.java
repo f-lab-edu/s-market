b7260b23249535b3dece26491c09d1fb085093b0
sangyunpark99 <psangyun5108@gmail.com>
2025-05-07 09:31:37 +0900

    feat: 재고 증가/감소 Kafka 컨슈머 분리 및 중복 처리 키, Outbox 업데이트 로직 개선 (#13)

--- product/src/main/java/com/sangyunpark/product/infrastructure/kafka/StockEventConsumer.java
+++ product/src/main/java/com/sangyunpark/product/infrastructure/kafka/StockEventConsumer.java
@@ -1,9 +1,10 @@
 package com.sangyunpark.product.infrastructure.kafka;
 
-import com.sangyunpark.product.application.event.StockDeductedEvent;
+import com.sangyunpark.product.infrastructure.kafka.event.StockDeductedEvent;
 import com.sangyunpark.product.constant.ErrorCode;
 import com.sangyunpark.product.constant.OutboxStatus;
 import com.sangyunpark.product.exception.BusinessException;
+import com.sangyunpark.product.infrastructure.kafka.event.StockIncreasedEvent;
 import com.sangyunpark.product.infrastructure.redis.OrderDuplicationRepository;
 import com.sangyunpark.product.infrastructure.repository.StockJpaRepository;
 import com.sangyunpark.product.infrastructure.repository.StockOutboxRepository;
@@ -12,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 
@@ -21,20 +21,22 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class StockEventConsumer {
 
-    private final String TOPIC = "stock.deducted";
-    private final String GROUP_ID = "product-service";
+    private final String DECR_TOPIC = "stock.deducted";
+    private final String INCR_TOPIC = "stock.increased";
+    private final String DECR_GROUP_ID = "product-service-decrease";
+    private final String INCR_GROUP_ID = "product-service-increase";
 
     private final OrderDuplicationRepository orderDuplicationRepository;
     private final StockJpaRepository stockJpaRepository;
     private final StockOutboxRepository stockOutboxRepository;
 
     @Transactional
-    @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
+    @KafkaListener(topics = DECR_TOPIC, groupId = DECR_GROUP_ID)
     public void consumeStockDeductedEvent(final StockDeductedEvent event) {
 
         final Long orderId = event.orderId();
 
-        if (!orderDuplicationRepository.saveIfAbsent(orderId, Duration.ofMinutes(3))) {
+        if (!orderDuplicationRepository.saveIfAbsentByOrderId(orderId, Duration.ofSeconds(30))) {
             log.info("이미 처리된 이벤트입니다. orderId={}", orderId);
             return;
         }
@@ -45,5 +47,24 @@ public class StockEventConsumer {
         }
 
         stockOutboxRepository.updateStatusByOrderId(event.orderId(), OutboxStatus.SEND, LocalDateTime.now());
+    }
+
+    @Transactional
+    @KafkaListener(topics = INCR_TOPIC, groupId = INCR_GROUP_ID)
+    public void consumeStockIncreasedEvent(final StockIncreasedEvent event) {
+
+        final String eventId = event.eventId();
+
+        if(!orderDuplicationRepository.saveIfAbsentByEventId(eventId, Duration.ofSeconds(30))) {
+            log.info("이미 처리된 이벤트입니다. eventId={}", eventId);
+            return;
+        }
+
+        int updatedRows = stockJpaRepository.increaseStock(event.productId(), event.quantity());
+        if (updatedRows == 0) {
+            throw new BusinessException(ErrorCode.PRODUCT_NOT_FOUND);
+        }
+
+        stockOutboxRepository.updateStatusByEventId(eventId, OutboxStatus.SEND, LocalDateTime.now());
     }
 }
