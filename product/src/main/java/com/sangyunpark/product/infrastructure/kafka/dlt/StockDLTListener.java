1fdbe96600d4df2b14f354de288f2961f9596acd
sangyunpark99 <psangyun5108@gmail.com>
2025-05-07 09:31:05 +0900

    chore: StockDeductedEvent 클래스 위치 변경 (#13)

--- product/src/main/java/com/sangyunpark/product/infrastructure/kafka/dlt/StockDLTListener.java
+++ product/src/main/java/com/sangyunpark/product/infrastructure/kafka/dlt/StockDLTListener.java
@@ -1,6 +1,6 @@
 package com.sangyunpark.product.infrastructure.kafka.dlt;
 
-import com.sangyunpark.product.application.event.StockDeductedEvent;
+import com.sangyunpark.product.infrastructure.kafka.event.StockDeductedEvent;
 import com.sangyunpark.product.infrastructure.redis.StockRedisRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
