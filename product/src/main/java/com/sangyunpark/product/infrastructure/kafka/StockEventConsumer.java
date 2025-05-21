23a6ee71938a18752bcf8e8e9006747e87aabfe8
sangyunpark99 <psangyun5108@gmail.com>
2025-05-21 13:20:07 +0900

    feat: Kafka 이벤트 중복 소비 방지를 위한 orderId 기반 중복 체크 로직 추가 (#15)

--- product/src/main/java/com/sangyunpark/product/infrastructure/kafka/StockEventConsumer.java
+++ product/src/main/java/com/sangyunpark/product/infrastructure/kafka/StockEventConsumer.java
@@ -3,12 +3,15 @@ package com.sangyunpark.product.infrastructure.kafka;
 import com.sangyunpark.product.application.event.StockDeductedEvent;
 import com.sangyunpark.product.constant.ErrorCode;
 import com.sangyunpark.product.exception.BusinessException;
+import com.sangyunpark.product.infrastructure.redis.OrderDuplicationRepository;
 import com.sangyunpark.product.infrastructure.repository.StockJpaRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
 
 @Slf4j
 @Component
@@ -19,10 +22,15 @@ public class StockEventConsumer {
     private final String GROUP_ID = "product-service";
 
     private final StockJpaRepository stockJpaRepository;
+    private final OrderDuplicationRepository orderDuplicationRepository;
 
     @Transactional
     @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
     public void consumeStockDeductedEvent(final StockDeductedEvent event) {
+
+        if(!orderDuplicationRepository.saveIfAbsent(event.orderId(), Duration.ofMinutes(5))) {
+            throw new BusinessException(ErrorCode.DUPLICATE_ERROR);
+        }
 
         int updatedRows = stockJpaRepository.decreaseStock(event.productId(), event.quantity());
         if(updatedRows == 0) {
