ed1eb89f3294db631fe26083fd4540ccb16f93f5
sangyunpark99 <psangyun5108@gmail.com>
2025-05-21 13:17:01 +0900

    feat: ProductJpaRepository 구현 (#15)

--- product/src/main/java/com/sangyunpark/product/infrastructure/repository/ProductJpaRepository.java
+++ product/src/main/java/com/sangyunpark/product/infrastructure/repository/ProductJpaRepository.java
@@ -2,7 +2,13 @@ package com.sangyunpark.product.infrastructure.repository;
 
 import com.sangyunpark.product.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT p.id FROM Product p WHERE p.id IN :ids")
+    List<Long> findExistingIds(@Param("ids") List<Long> ids);
 }
