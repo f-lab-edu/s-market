1b86f172fafa84bc7d55ee027fd664981adac088
sangyunpark99 <psangyun5108@gmail.com>
2025-05-21 13:18:39 +0900

    feat: StockClient에 Resilience4j CircuitBreaker 및 Retry 적용 (#15)

---
+++ order/src/main/java/com/sangyunpark/order/client/StockClient.java
@@ -0,0 +1,21 @@
+package com.sangyunpark.order.client;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+@FeignClient(name = "stock-service", url = "${client.stock-service.url}")
+public interface StockClient {
+
+    @Retry(name = "stockService")
+    @CircuitBreaker(name = "stockService")
+    @GetMapping("/exists")
+    Map<Long, Long> getQuantitiesByProductIds(final @RequestBody List<Long> productsId);
+}
